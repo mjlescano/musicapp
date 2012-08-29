7a5e55391e003d66bd54d971fbb524eb793b9518
Matias Lescano <matiasj.lescano@gmail.com>
2012-08-29 16:34:27 -0300

    tuff

--- main.php
+++
@@ -1,9 +0,0 @@
-<?php require_once './faceFunctions.php'; ?> 
-<html>
-	<head>
-		<title>Musicapp</title>
-	</head>
-	<body>
-		<a href="<?php echo $loginUrl; ?>" target="_blank">Login</a>
-	</body>
-</html>
